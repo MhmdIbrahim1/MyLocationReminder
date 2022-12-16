b209b04f49ec034ff0dca0dae39d0b5e976be555
MhmdIbrahim1 <S2051710242EG@std.aou.edu.eg>
2022-12-16 06:32:33 +0200

    getReminders
    -first step  fill  getReminders

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,13 +4,20 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Test Exception")
+        }
+        reminders?.let {
+            return Result.Success(ArrayList(it))
+        }
+        return Result.Error("No reminders found")
     }
+
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
         TODO("save the reminder")
