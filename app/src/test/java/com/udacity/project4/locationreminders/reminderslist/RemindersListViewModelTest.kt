cb4e10bd9d645fd9f1b3822c0865b2e5456df90c
MhmdIbrahim1 <S2051710242EG@std.aou.edu.eg>
2022-12-16 07:39:59 +0200

    Update RemindersListViewModelTest.kt
    first test

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,11 +1,53 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    @get: Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @get: Rule
+    var mainCoroutineRule = MainCoroutineRule()
+    private lateinit var remindersListViewModel: RemindersListViewModel
+    private lateinit var datasource: FakeDataSource
+
+
+    @Before
+    fun setUp() {
+        stopKoin()
+        datasource = FakeDataSource()
+        remindersListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), datasource)
+    }
+
+    @Test
+    fun check_loading() = mainCoroutineRule.runBlockingTest {
+        mainCoroutineRule.pauseDispatcher()
+        remindersListViewModel.loadReminders()
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            Matchers.`is`(true)
+        )
+        mainCoroutineRule.resumeDispatcher()
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            Matchers.`is`(false)
+        )
+    }
 }
